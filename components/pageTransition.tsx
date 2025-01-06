49cbb0424b76e68756160fa6d3c571325360c556
Prabhat-7 <prabhatsitaula@gmail.com>
2025-01-06 08:18:53 +0545

    added navbar

--- components/pageTransition.tsx
+++ components/pageTransition.tsx
@@ -5,39 +5,8 @@ import { usePathname } from "next/navigation";
 
 const PageTransition = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
-  const [prevPath, setPrevPath] = useState(pathname);
 
-  const getDirection = () => {
-    if (prevPath.length < pathname.length) return -100;
-    return 100;
-  };
-
-  useEffect(() => {
-    setPrevPath(pathname);
-  }, [pathname]);
-
-  return (
-    <AnimatePresence initial={false} mode="popLayout">
-      <motion.div
-        key={pathname}
-        initial={{ x: getDirection() }}
-        animate={{ x: 0 }}
-        exit={{ x: -getDirection() }}
-        transition={{
-          type: "spring",
-          stiffness: 400,
-          damping: 50,
-        }}
-        style={{
-          position: "absolute",
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
+  return <AnimatePresence initial={false} mode="popLayout"></AnimatePresence>;
 };
 
 export default PageTransition;
